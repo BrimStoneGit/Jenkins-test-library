e50b6953cbd37167df844894c98c3d5204351d53
Benjamin Dornfeld <benjamin.dornfeld@rommelag.com>
2024-07-12 15:43:29 +0200

    Added fileURL and fileName as parameters

--- vars/downloadFile.groovy
+++ vars/downloadFile.groovy
@@ -3,12 +3,9 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 @NonCPS
-def call() {
-   // URL of the file to download
-    def fileUrl = 'https://ash-speed.hetzner.com/10GB.bin'
-
+def call(String fileUrl, String fileName) {
     // Define the file path within the workspace
-    def downloadFilePath = "${env.WORKSPACE}/10GB.bin"
+    def downloadFilePath = "${env.WORKSPACE}/${fileName}"
 
     // Use pipeline utility steps to download the file
     def fileDownloader = new URL(fileUrl).openStream()
