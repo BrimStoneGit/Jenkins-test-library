b924d926c361c84cf37460c63e76ee684faf32e3
Benjamin Dornfeld <benjamin.dornfeld@rommelag.com>
2024-07-12 14:28:33 +0200

    adjusted downloadFile function

--- vars/downloadFile.groovy
+++ vars/downloadFile.groovy
@@ -4,15 +4,18 @@ import java.nio.file.Paths
 
 @NonCPS
 def call() {
-    // Define the URL of the file to download
-    def fileUrl = "https://ash-speed.hetzner.com/100MB.bin"
-    
-    // Define the name of the file to save locally
-    def fileName = "downloads/100MB.bin"
-    
-    // Open a stream from the URL and save it to the file
-    new URL(fileUrl).withInputStream { inputStream ->
-        Files.copy(inputStream, Paths.get(fileName))
-        println "File downloaded and saved as $fileName"
+   // URL of the file to download
+    def fileUrl = 'https://ash-speed.hetzner.com/100MB.bin'
+
+    // Define the file path within the workspace
+    def downloadFilePath = "${env.WORKSPACE}/100MB.bin"
+
+    // Use pipeline utility steps to download the file
+    def fileDownloader = new URL(fileUrl).openStream()
+    new File(downloadFilePath).withOutputStream { out ->
+        out << fileDownloader
     }
+    fileDownloader.close()
+
+    echo "File downloaded successfully at ${downloadFilePath}"
 }
